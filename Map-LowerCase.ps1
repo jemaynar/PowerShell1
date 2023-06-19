dfab0c93556da355ad6bccc6e8ea514b31cfc9d8
John Maynard <maynard.john.e@gmail.com>
2023-06-19 14:07:20 -0400

    Map-LowerCase.ps1 - maps string array to lowercase.

---
+++ Map-LowerCase.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.Synopsis
+Transform a collection to lowercase
+.Description
+Returns lowercase version of passed in array
+.Parameter strings
+The strings to be transformed
+.Example
+c:\Map-LowerCase.ps1 -strings "HELLO","heLlO","Hello"
+#>
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$True)]
+    [string[]]$strings
+)
+
+Write-Verbose "Passed in strings were $strings"
+
+$strings | Select-Object -Property @{name='Name';expression = {$_.ToLower()} } | Select-Object -ExpandProperty Name
+
+Write-Verbose "Finished processing strings"
