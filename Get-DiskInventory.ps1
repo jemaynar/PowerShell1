8f8934e9e58c62e55f4285d21e3bae9653ce1e2a
John Maynard <maynard.john.e@gmail.com>
2023-06-19 16:43:33 -0400

    Update comments.

--- Get-DiskInventory.ps1
+++ Get-DiskInventory.ps1
@@ -1,15 +1,13 @@
 ﻿<#
 .SYNOPSIS
-Get-DiskInventory retrieves logical disk information from one or
-
-more computers.
+Get-DiskInventory retrieves logical disk information from a computer.
 .DESCRIPTION
 Get-DiskInventory uses WMI to retrieve the Win32_LogicalDisk
 instances from one or more computers. It displays each disk's
 drive letter, free space, total size, and percentage of free
 space.
 .PARAMETER computername
-The computer name, or names, to query. Default: Localhost.
+The computer name, to query. Default: Localhost.
 .PARAMETER drivetype
 The drive type to query. See Win32_LogicalDisk documentation
 for values. 3 is a fixed disk, and is the default.
