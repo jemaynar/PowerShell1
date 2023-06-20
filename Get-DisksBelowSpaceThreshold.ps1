6914f9ba3161a53fecfc5554bc22fb1c494268f3
John Maynard <maynard.john.e@gmail.com>
2023-06-19 20:08:12 -0400

    Update comment in Get-DisksBelowSpaceThreshold.ps.  Redundant use of local.

--- Get-DisksBelowSpaceThreshold.ps1
+++ Get-DisksBelowSpaceThreshold.ps1
@@ -12,7 +12,7 @@ Get-DisksBelowSpaceThreshold -minimum 20
 Find all disks on the localhost with less than 20% free space.
 .Example
 .\Get-DisksBelowSpaceThreshold.ps1 -comp="localhost","DESKTOP1" -minimum 99
-Find all local disks on localhost & DESKTOP1 with less than 99% free space.
+Find all disks on localhost & DESKTOP1 with less than 99% free space.
 .Example
 .\Get-DisksBelowSpaceThreshold
 Find all disks on localhost with less than 10% free space.
