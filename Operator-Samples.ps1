a2e89f101f20a04802ff52eb6d3efea9d35322e3
John Maynard <maynard.john.e@gmail.com>
2023-06-21 23:27:50 -0400

    Adding String replace sample.

--- Operator-Samples.ps1
+++ Operator-Samples.ps1
@@ -1,6 +1,11 @@
+# Type Conversion Example
 1000 / 3 -as [int] # 333
 
+# Type Test Examples
 123.45 -is [int] # False
 "SERVER-R2" -is [string] # True
 $True -is [bool] # True
 (Get-Date) -is [datetime] # True
+
+# String replacement
+"192.168.34.12" -replace "34","15" # 192.168.15.12
