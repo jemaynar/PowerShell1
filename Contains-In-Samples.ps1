63aff430a7bcdc992108d220b0c3b8eb874f4ee0
John Maynard <maynard.john.e@gmail.com>
2023-07-09 07:53:04 -0400

    Contains & In code samples.

---
+++ Contains-In-Samples.ps1
@@ -0,0 +1,10 @@
+﻿# -contains intended to be used with collections not strings.
+'this' -contains '*his*' #False
+
+# -like is for string wildcards
+'this' -like '*his' #True
+
+# -contains correct usage
+$collection = 'abc','def','ghi'
+$collection -contains 'abc' #True
+$collection -contains 'pop-tarts' #False
