c8e37b6f2aae0f6b23f09b355b80bcaf137a6f3f
John Maynard <maynard.john.e@gmail.com>
2023-07-09 07:42:44 -0400

    How to use Split and Join Sample.

---
+++ Split-Join-Samples.ps1
@@ -0,0 +1,11 @@
+﻿#declare an array
+$array = "one","two","three","four","five"
+$array
+
+#combine array to single string using join 
+$array = $array -join "|"
+$array
+
+#inverse: split the string back into array
+$array = $array -split "\|"
+$array
